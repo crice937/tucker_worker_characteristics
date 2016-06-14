953c57ffb46e4ab0858c9734c22124c4a2159556
Chris Rice <xiong937@gmail.com>
2016-06-14 12:14:23 -0400

    adding files

---
+++ js/maps.js
@@ -0,0 +1,53 @@
+var dojoConfig = {
+  packages: [{
+    name: "bootstrap",
+    location: "https://esri.github.io/calcite-maps/dist/vendor/dojo-bootstrap"
+  },
+  {
+    name: "calcite-maps",
+    location: "https://esri.github.io/calcite-maps/dist/js/dojo"
+  }]
+};
+
+require([
+      // ArcGIS JS
+      "esri/views/MapView",
+      "esri/WebMap",
+      "esri/widgets/Legend",
+
+      // Bootstrap
+      "bootstrap/Collapse", 
+
+      // Calcite-maps
+      "calcite-maps/calcitemaps-v0.2",
+      "dojo/domReady!"
+      ], function(MapView, WebMap, Legend) {
+
+      // Webmap 
+      var webmap = new WebMap({
+        portalItem: {
+          id: "6de0c1835caa41ff893505c003123f6d"
+        }
+      });
+
+      // View
+      var view = new MapView({
+        map: webmap,
+        container: "mapViewDiv",
+        padding: {
+          top: 64
+        }
+      });
+
+      // Legend
+      view.then(function(result) {
+        var legend = new Legend({
+          view: view,
+          layerInfos: [{
+            layer: view.map.layers.items[0],
+            title: ""
+          }]
+        });
+        view.ui.add(legend, "top-right");
+      });
+    });
